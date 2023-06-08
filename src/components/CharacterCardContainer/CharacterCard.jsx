95944ef873745a083d0cf511f045fca94a1df65e
Matt-Hulme <Mattwhulme@gmail.com>
2023-06-07 19:39:11 -0600

    People API Request 1

--- src/components/CharacterCardContainer/CharacterCard.jsx
+++ src/components/CharacterCardContainer/CharacterCard.jsx
@@ -1,7 +1,11 @@
-export default function CharacterCard() {
-    return (
-        <div className="CharacterCard">
-        </div>
-    );
-  }
-  
+import React from 'react';
+
+export default function CharacterCard({ character }) {
+  return (
+    <div className="CharacterCard">
+      <h3>{character.name}</h3>
+      <p>Homeworld: {character.homeworld}</p>
+      <p>Films: {character.films.join(', ')}</p>
+    </div>
+  );
+}
--- src/components/CharacterCardContainer/CharacterCardContainer.jsx
+++ src/components/CharacterCardContainer/CharacterCardContainer.jsx
@@ -1,7 +1,36 @@
+import React, { useEffect, useState } from 'react';
+import CharacterCard from './CharacterCard';
+
 export default function CharacterCardContainer() {
-    return (
-        <div className="CharacterCardContainer">
-        </div>
-    );
-  }
-  
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    const fetchCharacters = async () => {
+        try {
+          let allCharacters = [];
+          let nextPage = 'https://swapi.dev/api/people/';
+      
+          while (nextPage) {
+            const response = await fetch(nextPage);
+            const data = await response.json();
+            allCharacters = [...allCharacters, ...data.results];
+            nextPage = data.next;
+          }
+      
+          setCharacters(allCharacters);
+        } catch (error) {
+          console.error('Error fetching characters:', error);
+        }
+      };
+
+    fetchCharacters();
+  }, []);
+
+  return (
+    <div className="CharacterCardContainer">
+      {characters.map((character, index) => (
+        <CharacterCard key={index} character={character} />
+      ))}
+    </div>
+  );
+}

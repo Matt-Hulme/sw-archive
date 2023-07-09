a508073b6f4c4b14ccf28bccc598d6fc5819ccfe
Matt-Hulme <Mattwhulme@gmail.com>
2023-07-09 09:39:16 -0600

    17th Character Skipping Correctly

--- src/components/CharacterDataFetcher.jsx
+++ src/components/CharacterDataFetcher.jsx
@@ -4,36 +4,29 @@ export default function CharacterDataFetcher({ children }) {
   const [characterData, setCharacterData] = useState([]);
 
   useEffect(() => {
-    const cachedData = localStorage.getItem('characterData');
-    if (cachedData) {
-      const parsedData = JSON.parse(cachedData);
-      if (Array.isArray(parsedData) && parsedData.length > 0) {
-        setCharacterData(parsedData);
-      } else {
-        fetchAllCharacters();
-      }
-    } else {
-
-      fetchAllCharacters();
-    }
+    fetchAllCharacters();
   }, []);
 
   const fetchAllCharacters = async () => {
     try {
       let allCharacters = [];
       let nextUrl = 'https://swapi.dev/api/people/';
-      let id = '0';
+      let id = 0;
 
       while (nextUrl) {
         const response = await fetch(nextUrl);
         const data = await response.json();
         const characters = data.results.map((character) => {
-          const characterId = id === '17' ? '18' : id;
-          id = (parseInt(id) + 1).toString();
-          console.log('character.id', characterId);
+          if (id === 16) {
+            id++;
+            
+          }
+
+          const characterId = id++;
+          
           return {
             ...character,
-            ['id']: characterId,
+            id: characterId,
           };
         });
         allCharacters.push(...characters);

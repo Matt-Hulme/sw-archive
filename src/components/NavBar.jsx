a7742712ac805e857eee2e5cb9bf510d94c393e2
Matt-Hulme <Mattwhulme@gmail.com>
2023-07-23 20:52:53 -0600

    NavBar Started

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,14 @@
 export default function NavBar() {
     return (
-        <>
-        </>
+        <div className="NavBar">
+            <ul>
+                <li><a>Characters</a>
+                <li><a>Films</a></li>
+                <li><a>Species</a></li>
+                <li><a>Starships</a></li>
+                <li><a>Vehicles</a></li>
+                <li><a>Planets</a></li></li>     
+            </ul>
+        </div>
     );
 }
--- src/components/Pages/HomePage.jsx
+++ src/components/Pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import CharacterCardContainer from 'src/Components/CharacterCardContainer/CharacterCardContainer.jsx';
+import NavBar from '../NavBar.jsx';
 import 'src/App.css'
 
 export default function HomePage() {
   return (
     <div className="HomePage">
-      <CharacterCardContainer />
+        <NavBar />
+        <CharacterCardContainer />
     </div>
   );
 }

6ad226651409fd63988ad38d4d9e4de8a1101c51
Matt-Hulme <Mattwhulme@gmail.com>
2023-08-23 21:46:18 -0600

    HomePage Links Working :O

--- src/components/Pages/HomePage.jsx
+++ src/components/Pages/HomePage.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function HomePage() {
   return (
     <div className="HomePage">
       <div className="HomePageContainer">
-          <li className="HomePageCharacters">Characters!</li>
-          <li className="HomePageFilms">Films</li>
-          <li className="HomePagePlanets">Planets</li>
-          <li className="HomePageSpecies">Species</li>
-          <li className="HomePageVehicles">Vehicles</li>
-          <li className="HomePageStarships">Starships</li>
+        <Link to={`/characters`} className="HomePageCharacters">
+          Characters
+        </Link>
+        <Link to={`/films`} className="HomePageFilms">
+          Films
+        </Link>
+        <Link to={`/planets`} className="HomePagePlanets">
+          Planets
+        </Link>
+        <Link to={`/species`}className="HomePageSpecies">
+          Species
+        </Link>
+        <Link to={`/vehicles`} className="HomePageVehicles">
+          Vehicles
+        </Link>
+        <Link to={`/starships`} className="HomePageStarships">
+          Starships
+        </Link>
       </div>
     </div>
   );

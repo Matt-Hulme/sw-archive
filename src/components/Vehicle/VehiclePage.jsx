5d62c4c7f15082ca6df51addcfe42d500476541e
Matt-Hulme <Mattwhulme@gmail.com>
2023-09-27 20:57:49 -0600

    Forgot 1 useMemo Import

--- src/components/Vehicle/VehiclePage.jsx
+++ src/components/Vehicle/VehiclePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { useParams, useLocation, Link } from 'react-router-dom';
 import VehicleImageArray from './VehicleImageArray';
 import CharacterImageArray from '../Character/CharacterImageArray';
